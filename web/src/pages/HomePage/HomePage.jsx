7c9f466380c49419eb9850ca45cf8165b7ddcebf
BrendanCreates <bnevilsstem2023@gmail.com>
2025-02-25 09:16:48 -0500

    Created about page, working on layour
    9:20 in Chapter 1 video

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -6,14 +6,7 @@ const HomePage = () => {
     <>
       <Metadata title="Home" description="Home page" />
 
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.jsx</code>
-      </p>
-      {/*
-           My default route is named `home`, link to me with:
-           `<Link to={routes.home()}>Home</Link>`
-        */}
+      <>Home</>
     </>
   )
 }
